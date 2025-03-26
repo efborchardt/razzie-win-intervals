2f12ab2803066da021f2416b140b76daf6226e49
efborchardt <evertonfelipeborchardt@gmail.com>
2025-03-26 10:48:53 -0300

    refactor: modify app initialization for testability

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,22 @@ import { DataLoadService } from "./services/data-load.service";
 import producerRouter from "./routes/producer.router";
 
 const app = express();
-const port = 3000;
 
-async function initializeApp() {
+export async function initializeApp() {
   await sequelize.sync({ force: true });
   const dataLoadingService = new DataLoadService();
   await dataLoadingService.loadDataFromCSV("./data/movielist.csv");
-
   app.use("/api", producerRouter);
-
-  app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-  });
+  return app;
 }
 
-initializeApp().catch(console.error);
+if (require.main === module) {
+  const port = 3000;
+  initializeApp().then(app => {
+      app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+      });
+    }).catch(console.error);
+}
+
+export default app;
