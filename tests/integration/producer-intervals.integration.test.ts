d3558331899cb812f351eb540a784bd3f6141bac
efborchardt <evertonfelipeborchardt@gmail.com>
2025-03-31 13:23:29 -0300

    fix(tests): ensure strict validation of producers win intervals based on default movielist.csv file

--- tests/integration/producer-intervals.integration.test.ts
+++ tests/integration/producer-intervals.integration.test.ts
@@ -4,7 +4,25 @@ import { initializeApp } from '../../src/app';
 import sequelize from '../../src/infrastructure/database';
 
 describe('API - Producers Integration test', () => {
-  const csvTestFilePath = "./data/movielist-test.csv";
+  const csvTestFilePath = "./data/movielist.csv";
+  const expectedResponse = {
+    min: [
+      {
+        producer: "Joel Silver",
+        interval: 1,
+        previousWin: 1990,
+        followingWin: 1991
+      }
+    ],
+    max: [
+      {
+        producer: "Matthew Vaughn",
+        interval: 13,
+        previousWin: 2002,
+        followingWin: 2015
+      }
+    ]
+  };
 
   let app: Express;
 
@@ -18,29 +36,29 @@ describe('API - Producers Integration test', () => {
 
   it('GET /api/producers-intervals - should return correct min/max intervals - 200', async () => {
     const res = await request(app).get('/api/producers-intervals');
-    
+
     expect(res.status).toBe(200);
     expect(res.body).toHaveProperty('min');
     expect(res.body).toHaveProperty('max');
     expect(Array.isArray(res.body.min)).toBeTruthy();
     expect(Array.isArray(res.body.max)).toBeTruthy();
-
-    res.body.min.forEach((entry: any) => {
-      expect(entry).toHaveProperty('producer');
-      expect(entry).toHaveProperty('interval');
-      expect(entry).toHaveProperty('previousWin');
-      expect(entry).toHaveProperty('followingWin');
-      expect(typeof entry.interval).toBe('number');
-      expect(entry.interval).toBeGreaterThanOrEqual(1);
-    });
-
-    res.body.max.forEach((entry: any) => {
-      expect(entry).toHaveProperty('producer');
-      expect(entry).toHaveProperty('interval');
-      expect(entry).toHaveProperty('previousWin');
-      expect(entry).toHaveProperty('followingWin');
-      expect(typeof entry.interval).toBe('number');
-      expect(entry.interval).toBeGreaterThanOrEqual(res.body.min[0].interval);
-    });
+    expect(res.body.min.length).toBe(1);
+    expect(res.body.max.length).toBe(1);
+    assertMinMaxIntervalResponse(res.body.min[0], expectedResponse.min[0]);
+    assertMinMaxIntervalResponse(res.body.max[0], expectedResponse.max[0]);
   });
 });
+
+function assertMinMaxIntervalResponse(entry: any, expectedEntry: { producer: string; interval: number; previousWin: number; followingWin: number; }) {
+  expect(entry).toHaveProperty('producer');
+  expect(entry.producer).toBe(expectedEntry.producer);
+
+  expect(entry).toHaveProperty('interval');
+  expect(entry.interval).toBe(expectedEntry.interval);
+
+  expect(entry).toHaveProperty('previousWin');
+  expect(entry.previousWin).toBe(expectedEntry.previousWin);
+
+  expect(entry).toHaveProperty('followingWin');
+  expect(entry.followingWin).toBe(expectedEntry.followingWin);
+}
